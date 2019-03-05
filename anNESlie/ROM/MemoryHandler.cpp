8a87ed2bdb51a3d8934c83cb960ed29271885f70
Loyie King <loyie924558375@hotmail.com>
2019-03-05 23:22:19 +0800

    修改Memory类为MemoryHandler类

---
+++ anNESlie/ROM/MemoryHandler.cpp
@@ -0,0 +1,75 @@
+#include "MemoryHandler.h"
+
+using namespace ROM;
+
+MemoryHandler::MemoryHandler(char* memory_adress, int memory_size)
+{
+	memory = memory_adress;
+	readHandlers = new ReadHandler[memory_size];
+	writeHandlers = new WriteHandler[memory_size];
+	for (int i = 0; i < memory_size; i++)
+	{
+		memory = 0;
+		readHandlers = nullptr;
+		writeHandlers = nullptr;
+	}
+}
+
+MemoryHandler::~MemoryHandler()
+{
+	if (readHandlers == nullptr)
+		return;
+	delete[] readHandlers;
+	delete[] writeHandlers;
+	readHandlers = nullptr;
+}
+
+inline void ROM::MemoryHandler::SetReadHandler(int adress, ReadHandler handler)
+{
+	readHandlers[adress] = handler;
+}
+
+inline void ROM::MemoryHandler::SetReadHandler(int adress_start, int adress_end, ReadHandler handler)
+{
+	for (int i = adress_start; i < adress_end; i++)
+		SetReadHandler(i, handler);
+}
+
+inline void ROM::MemoryHandler::SetWriteHandler(int adress, WriteHandler handler)
+{
+	writeHandlers[adress] = handler;
+}
+
+inline void ROM::MemoryHandler::SetWriteHandler(int adress_start, int adress_end, WriteHandler handler)
+{
+	for (int i = adress_start; i < adress_end; i++)
+		SetWriteHandler(i, handler);
+}
+
+inline char ROM::MemoryHandler::ReadByte(int adress)
+{
+	if (readHandlers[adress] == nullptr)
+		return memory[adress];
+	else
+		return readHandlers[adress](adress);
+}
+
+inline void ROM::MemoryHandler::WriteByte(int adress, char value)
+{
+	if (writeHandlers[adress] == nullptr)
+		memory[adress] = value;
+	else
+		writeHandlers[adress](adress, value);
+}
+
+short ROM::MemoryHandler::ReadWord(int adress)
+{
+	return ((short)ReadByte(adress)) | (((short)ReadByte(adress + 1)) << 8);
+}
+
+void ROM::MemoryHandler::WriteWord(int adress, short value)
+{
+	WriteByte(adress, value);
+	WriteByte(adress + 1, value >> 8);
+}
+
---
+++ anNESlie/ROM/MemoryHandler.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+namespace ROM
+{
+	/*
+		此类的用处：
+			1.NES的内存不是简单地线性关系，存在大量内存之间的映射、Mapper，所以创建这一个类来统一处理这类问题
+			2.不在此类内直接new出内存而是交给外部管理，是为了简化ROM的Mapper交换过程（本类因此被称为Handler）
+	*/
+
+	/*
+	NES为典型的八位机，不仅寄存器、内存每一位的长度同样为8bit，所以应该采用char
+	*/
+	typedef char(*ReadHandler)(int adress);
+	typedef void(*WriteHandler)(int adress, char value);
+
+
+	class MemoryHandler
+	{
+	private:
+		char* memory;
+		ReadHandler *readHandlers;
+		WriteHandler *writeHandlers;
+
+	public:
+		MemoryHandler(char* memory_adress,int memory_size);
+		~MemoryHandler();
+
+		inline void SetReadHandler(int adress, ReadHandler handler);
+		inline void SetReadHandler(int adress_start, int adress_end, ReadHandler handler);
+		inline void SetWriteHandler(int adress, WriteHandler handler);
+		inline void SetWriteHandler(int adress_start, int adress_end, WriteHandler handler);
+
+		inline char ReadByte(int adress);
+		inline void WriteByte(int adress, char value);
+		
+		short ReadWord(int adress);
+		void WriteWord(int adress, short value);
+
+	};
+}
